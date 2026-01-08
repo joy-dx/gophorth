fba34bed2cd36cc207cf9b2dc5b4d65a934c9250
symball <contact@simonball.me>
2026-01-08 14:11:17 +0800

    * Align state export keys

--- pkg/updater/updaterdto/models.go
+++ pkg/updater/updaterdto/models.go
@@ -8,20 +8,20 @@ import (
 )
 
 type UpdaterState struct {
-	Architecture    string                    `json:"architecture"`
+	Architecture    string                    `json:"updater_architecture"`
 	Changelog       string                    `json:"updater_changelog"`
 	CheckInterval   time.Duration             `json:"updater_check_interval"`
 	LastUpdateCheck *time.Time                `json:"updater_last_update_check" ts_type:"string"`
 	Log             string                    `json:"updater_log"`
 	LogPath         string                    `json:"updater_log_path"`
-	Platform        string                    `json:"platform"`
-	PublicKey       string                    `json:"public_key"`
-	PublicKeyPath   string                    `json:"public_key_path"`
+	Platform        string                    `json:"updater_platform"`
+	PublicKey       string                    `json:"updater_public_key"`
+	PublicKeyPath   string                    `json:"updater_public_key_path"`
 	ReleasedAt      *time.Time                `json:"updater_released_at" ts_type:"string"`
-	TemporaryPath   string                    `json:"temporary_path"`
+	TemporaryPath   string                    `json:"updater_temporary_path"`
 	UpdateLink      *releaserdto.ReleaseAsset `json:"updater_update_link"`
 	Updating        bool                      `json:"updater_updating"`
-	Variant         string                    `json:"variant"`
+	Variant         string                    `json:"updater_variant"`
 	Version         string                    `json:"updater_version" yaml:"updater_version"`
 }
 
